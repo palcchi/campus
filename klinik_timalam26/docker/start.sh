7390e6c63c08c8b6736c0b979ad1d4c4d09adef0
palcchi <valliantito99@gmail.com>
2026-09-23 20:04:58 +0700

    Fix CodeIgniter container host binding

--- klinik_timalam26/docker/start.sh
+++ klinik_timalam26/docker/start.sh
@@ -9,4 +9,4 @@ if [ ! -d vendor ] || [ ! -f vendor/autoload.php ]; then
   composer install --no-interaction --prefer-dist --optimize-autoloader
 fi
 
-exec php spark serve --host=0.0.0.0 --port=8080
+exec php spark serve --host 0.0.0.0 --port 8080
